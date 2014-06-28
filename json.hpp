97c0faec671fde70e84aff166c3d7a38cfa065e8
Matthew Sherborne <msherborne@gmail.com>
2014-06-29 04:44:05 +1000

    Playing with an iterator that will return the location of the error

--- json.hpp
+++ json.hpp
@@ -3,33 +3,37 @@
 #include "src/json_class.hpp"
 #include "src/mapper.hpp"
 #include "src/parser.hpp"
+#include "src/locatingIterator.hpp"
 #include <istream>
 #include <iterator>
 #include <algorithm>
+
 
 namespace json {
 
 JSON read(std::istream &in, bool skipOverErrors = false) {
   using Iterator = std::istream_iterator<char>;
-  JSONParser<Iterator> p(Iterator(in), Iterator(), skipOverErrors);
+  Iterator start = Iterator(in);
+  Iterator end;
+  JSONParser<LocatingIterator<Iterator>> p(locator(start), locator(end), skipOverErrors);
   return read(p);
 }
 
 JSON read(const std::string &in, bool skipOverErrors = false) {
-  JSONParser<std::string::const_iterator> p(in.cbegin(), in.cend(),
+  JSONParser<LocatingIterator<decltype(in.cbegin())>> p(locator(in.cbegin()), locator(in.cend()),
                                             skipOverErrors);
   return read(p);
 }
 
 JSON read(std::string&& in, bool skipOverErrors = false) {
-  JSONParser<std::string::const_iterator> p(in.cbegin(), in.cend(),
+  JSONParser<LocatingIterator<std::string::const_iterator>> p(locator(in.cbegin()), locator(in.cend()),
                                             skipOverErrors);
   return read(p);
 }
 
 
 JSON read(const char *begin, const char *end, bool skipOverErrors = false) {
-  JSONParser<const char*> p(begin, end, skipOverErrors);
+  JSONParser<LocatingIterator<const char*>> p(begin, end, skipOverErrors);
   return read(p);
 }
 
---
+++ src/locatingIterator.hpp
@@ -0,0 +1,33 @@
+/** An iterator wrapper that records a location in column and row coordinates */
+
+namespace json {
+
+template <typename T> struct LocatingIterator {
+  using MyType = LocatingIterator<T>;
+  T real;
+  int row;
+  int col;
+  MyType &operator++() {
+    if (*real == '\n') {
+      ++row;
+      col = 1;
+    } else {
+      ++col;
+    }
+    ++real;
+    return *this;
+  }
+  MyType operator++(int) {
+    MyType result = *this;
+    operator ++();
+    return result;
+  }
+  char operator*() { return *real; }
+  char &operator->() { return *real; }
+  bool operator !=(const MyType& other) const { return real != other.real; }
+  bool operator ==(const MyType& other) const { return real == other.real; }
+  LocatingIterator(T real) : real(real), row(1), col(1) {}
+  LocatingIterator(const MyType &other) : real(other.real), row(other.row), col(other.col) {}
+};
+
+}
