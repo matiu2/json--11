f7eafd0929fef75222811f3376e7c6fc7c6810c3
Matthew Sherborne <msherborne@gmail.com>
2014-07-30 11:36:48 +1000

    Made benchmark use char* instead of stream iterator

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -11,11 +11,14 @@ int main(int, char **) {
   std::ifstream f("benchmark.json");
   std::stringstream input;
   input << f.rdbuf();
+  std::string instr = input.str();
+  const char* start = instr.c_str();
+  const char* end = instr.c_str() + instr.size();
   // Parse it and print out check the friend's name 10k times
   const std::string expected("DixieSims");
   int retval = 0;
   for (long i = 0; i < 10000; ++i) {
-    json::JSON data = json::read(input.str());
+    json::JSON data = json::read(start, end);
     const json::JList &outter(data);
     const json::JList &friends = outter[0]["friends"];
     for (const json::JSON &myFriend : friends) {
